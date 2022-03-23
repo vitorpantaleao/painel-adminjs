6fb11e00a9fb897b1cb414b8d099b56a8a03b36e
Vitor Pantaleão <vitor-hmp@hotmail.com>
2022-03-22 22:17:06 -0300

    cria index database e modifica server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
+import "dotenv/config"
+import "./database"
 import AdminJS from "adminjs";
 import AdminJSExpress from '@adminjs/express'
+import AdminJSSequelize from '@adminjs/sequelize'
 import express from "express";
+
+AdminJS.registerAdapter(AdminJSSequelize)
 
 const app = express()
 
